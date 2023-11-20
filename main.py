768cc1c34f722dc12c26910ddc3e94f3f9623564
tomastaxa31 <134070185+tomastaxa31@users.noreply.github.com>
2023-11-20 17:39:08 +0000

    Bug fixes + Unlimited mode

--- main.py
+++ main.py
@@ -45,8 +45,15 @@ def test(args):
     max_inputs = args.max_value if not args.use_time_mode else float('inf')
 
     while True:
-        if time.time() - start_time > max_time or len(inputs) >= max_inputs:
-            break
+        if not args.unlimited:
+            if time.time() - start_time > max_time or len(inputs) >= max_inputs:
+                break
+
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            if key == b' ':
+                print("Test stopped by user.")
+                break
 
         random_input = generate_random_input("word" if args.use_words else "char")
         print(f"Input: {random_input}")
@@ -56,6 +63,9 @@ def test(args):
         while user_input is None and time.time() - start_time <= max_time:
             if msvcrt.kbhit():
                 user_input = msvcrt.getch().decode('utf-8').lower()
+
+        if user_input == " ":
+            break
 
         if user_input is None:
             break
@@ -92,9 +102,17 @@ def test(args):
     return inputs, test_start
 
 def calculate_test_statistics(inputs, test_start):
+    inputs2 = inputs.copy()
+    
     if not inputs:
         print("No inputs provided.")
         return
+    
+    for i in range(1, len(inputs)):
+        modified_duration = inputs[i].duration - inputs[i - 1].duration
+        inputs2[i] = inputs2[i]._replace(duration=modified_duration)
+
+    inputs = inputs2.copy()
 
     test_start = test_start
     test_end = datetime.now().strftime("%a %b %d %H:%M:%S %Y")
@@ -134,12 +152,20 @@ if __name__ == "__main__":
     parser.add_argument("-utm", "--use_time_mode", action="store_true", help="Use time mode.")
     parser.add_argument("-mv", "--max_value", type=int, help="Max number of seconds for time mode or maximum number of inputs for the number of inputs mode.")
     parser.add_argument("-uw", "--use_words", action="store_true", help="Use word typing mode, instead of single character typing.")
+    parser.add_argument("-unlimited", "--unlimited", action="store_true", help="Run the test indefinitely.")
 
     args = parser.parse_args()
 
-    if args.max_value is None:
-        parser.error("-mv is required.")
+    if args.unlimited and args.use_time_mode:
+        parser.error("-unlimited and -utm are mutually exclusive.")
 
+    if args.unlimited and args.max_value:
+        parser.error("-unlimited and -mv are mutually exclusive.")
+
+    if not args.unlimited and args.max_value is None:
+        parser.error("-mv is required unless -unlimited is specified.")
+
+    # Rest of the code remains the same
     inputs, test_start = test(args)
 
     print("\nTest completed.")
